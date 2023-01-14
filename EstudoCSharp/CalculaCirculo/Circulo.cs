72f10a2a66cf023ff9aca4f51654a4302ca3afa3
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2023-01-14 12:58:43 -0300

    starting project

---
+++ EstudoCSharp/CalculaCirculo/Circulo.cs
@@ -0,0 +1,22 @@
+﻿using EstudoCSharp;
+using System;
+using System.Globalization;
+
+namespace EstudoCSharp
+{
+    public static class CalculaCirculo
+    {
+        public static void AreaDoCirculo()
+        {
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ EstudoCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using EstudoCSharp;
+using System;
+using System.Globalization;
+
+namespace EstudoCSharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // CalculaTriangulo.AreaDoTriangulo();
+            // Estoque.VerificaEstoque();
+            CalculaCirculo.AreaDoCirculo();
+        }
+    }
+}
